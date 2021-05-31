ffb67eb5332692b6f944052bab5ef155e4ccb676
grafit_off <graf.off.it@gmail.com>
2021-06-01 00:15:13 +0300

    Added section Constructor

--- src/js/main.js
+++ src/js/main.js
@@ -69,5 +69,76 @@ document.addEventListener("DOMContentLoaded", () => {
 			}
 		}
 	});
-})
+
+
+	// Accodions
+	const accordionTrigger = document.querySelectorAll(".accordion__trigger"),
+		accordionBody = document.querySelectorAll(".accordion__body");
+
+	const accordionsToggle = (triggers, bodyElements) => {
+		triggers.forEach((el) => {
+			el.addEventListener("click", () => {
+				el.disabled = true;
+				setTimeout(() => {
+					el.disabled = false;
+				}, 500);
+				const elBody = el.closest(".accordion__item").querySelector(".accordion__body");
+				el.classList.toggle('accordion__trigger--active');
+				elBody.style.height = `${elBody.scrollHeight}px`;
+				if (elBody.style.height === "0px" || window.getComputedStyle(elBody).height === "0px") {
+					el.setAttribute("aria-expanded", "true");
+					elBody.setAttribute("aria-hidden", "false");
+				} else {
+					elBody.style.height = "0";
+					el.setAttribute("aria-expanded", "false");
+					elBody.setAttribute("aria-hidden", "true");
+				}
+			});
+		});
+		bodyElements.forEach((el) => {
+			el.addEventListener("transitionend", () => {
+				if (el.style.height !== "0px") {
+					el.style.height = "auto";
+				}
+			});
+		});
+	};
+	accordionsToggle(accordionTrigger, accordionBody);
+	// -- //
+
+	// Image Preview
+	/* 
+		Идея
+		По нажатию на картинку в списке, передавать src в выводимое изображение,
+		а src выводимого изображения передавть картинке по которой произошел клик.
+
+		План
+		1. Создать переменные выводимого изображения и блока списка картинок/кнопок.
+		2. Добавить слушатель на блок и способом делегирования отлавливать картинку
+			по которой произошел клик.
+		3. Получить в новые переменные src и alt картинки по которой произошел клик
+			и src, alt выводимого изображение.
+		4. Заменить src, alt выводимого изображения на src,alt картинки по которой произошел клик
+			из переменной и заменить src,alt картинки по которой произошел клик
+			на src,alt выводимого изображения.
+	*/
+	const mainImg = document.querySelector('.image-preview__current');
+	const imgList = document.querySelector('.image-preview__list');
+
+	imgList.addEventListener('click', (e) => {
+		let image = e.target;
+		let mainImgSrc = mainImg.getAttribute('src');
+		let mainImgAlt = mainImg.getAttribute('alt');
+
+		let imageSrc = image.getAttribute('src');
+		let imageAlt = image.getAttribute('alt');
+
+		mainImg.setAttribute('src', `${imageSrc}`);
+		mainImg.setAttribute('alt', `${imageAlt}`);
+
+		image.setAttribute('src', `${mainImgSrc}`);
+		image.setAttribute('alt', `${mainImgAlt}`);
+	})
+	// -- //
+});
 
