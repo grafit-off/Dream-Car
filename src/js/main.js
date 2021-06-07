47872afff980d8a0152e6aedd67d4ff4e13ee425
grafit_off <graf.off.it@gmail.com>
2021-06-08 01:12:44 +0300

    Form

--- src/js/main.js
+++ src/js/main.js
@@ -324,7 +324,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		generateOrderPrice();
 	}
 
-	// Shild 
+	// Schild 
 	const generateSchildItem = () => {
 		schildSum = parseInt(schildSpinner.value, 10) * 50;
 		if (parseInt(schildSpinner.value, 10) !== 0) {
@@ -384,7 +384,85 @@ document.addEventListener("DOMContentLoaded", () => {
 	})
 	// -- //
 
-	// Form Send
+	// Input Mask
+	const inputTel = document.querySelector('input[type="tel"]');
+	const inMask = new Inputmask('+7 (999) 999-99-99');
+	inMask.mask(inputTel);
+	// -- //
 
+	// Validate
+	const validate = (selector, rules) => {
+		new window.JustValidate(selector, {
+			rules: rules,
+			messages: {
+				name: {
+					required: 'Это поле обязательно!'
+				},
+				auto: {
+					required: 'Это поле обязательно!'
+				},
+				tel: {
+					required: 'Это поле обязательно!'
+				}
+			},
+			submitHandler: (form, value, ajax) => {
+				console.log(form);
+			}
+		})
+	};
+
+	/* validate('.form', {
+		name: { required: true }, auto: { required: true }, tel: { required: true }
+	}) */
+	// -- //
+
+	// Form Send
+	const form = document.querySelector('.form');
+
+	form.addEventListener('submit', async (e) => {
+		e.preventDefault();
+		const self = e.currentTarget;
+		const formData = new FormData(self);
+
+
+		const name = document.querySelector('input[name="Имя"]').value;
+		const auto = document.querySelector('input[name="Модель автомобиля"]').value;
+		const tel = inputTel.value;
+		const heel = heelInp.value;
+		const frontRow = frontRowInp.value;
+		const backRow = backRowInp.value;
+		const trunk = trunkInp.value;
+		const schild = schildSpinner.value;
+
+		formData.append('Имя', JSON.stringify(name));
+		formData.append('Марка авто', auto);
+		formData.append('Номер телефона', tel);
+		formData.append('Металлический подпятник', heel);
+		formData.append('Передний ряд', frontRow);
+		formData.append('Задний ряд', backRow);
+		formData.append('Багажник ', trunk);
+		formData.append('Фирменные шильды .шт', schild);
+
+		let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+			method: 'POST',
+			body: formData
+		})
+
+		let result = await response.json();
+		console.log(result);
+	})
 });
 
+/* const xhr = new XMLHttpRequest();
+		xhr.addEventListener('readystatechange', () => {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					console.log('Отправлено');
+				}
+			}
+		});
+		xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts', true);
+		xhr.send(formData);
+		xhr.onload = function () {
+			alert(`Загружено: ${xhr.status} ${xhr.response}`);
+		}; */
